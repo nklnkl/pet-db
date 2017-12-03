4860ca764b7c83dae8be9e46c9b6ce6e22c1c733
Niko Lagman <Lagman.Niko@gmail.com>
2017-12-03 06:40:44 -0500

    Update index

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
 import * as Dotenv from 'dotenv';
 import { Client } from './client';
+import { AccountDb } from './service/account';
+import { PetDb } from './service/pet';
+import { SessionDb } from './service/session';
+import { AccountSchema, AccountInterface } from './schema/account';
+import { PetSchema, PetInterface } from './schema/pet';
+import { SessionSchema, SessionInterface } from './schema/session';
 
 Dotenv.config();
 
