821095a89ab98db5faaea7d6e9d56bd537b7230f
Niko Lagman <Lagman.Niko@gmail.com>
2017-12-04 08:50:58 -0500

    Rename ts files

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 import * as Dotenv from 'dotenv';
 import { Client } from './client';
-import { AccountDb } from './service/account';
-import { PetDb } from './service/pet';
-import { SessionDb } from './service/session';
-import { AccountSchema, AccountInterface } from './schema/account';
-import { PetSchema, PetInterface } from './schema/pet';
-import { SessionSchema, SessionInterface } from './schema/session';
+import { AccountDb } from './service/account.service';
+import { PetDb } from './service/pet.service';
+import { SessionDb } from './service/session.service';
+import { AccountSchema, AccountInterface } from './schema/account.schema';
+import { PetSchema, PetInterface } from './schema/pet.schema';
+import { SessionSchema, SessionInterface } from './schema/session.schema';
 
 Dotenv.config();
 
